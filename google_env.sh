aadb1930d992042127fbcc0832947050bae37f1b
michael-conway <mike.conway@nih.gov>
2021-03-08 14:59:00 -0500

    #1 before upgrade

--- google_env.sh
+++
@@ -1,3 +0,0 @@
-export GOOGLE_PROJECT_ID="nih-niehs-dntp-ods-dev"
-export GOOGLE_APPLICATION_CREDENTIALS="./Secrets/nih-niehs-dntp-ods-dev-1656c15eb6fc.json"
-export GOOGLE_ADMIN_EMAIL="gen3-storage-service-account@nih-niehs-dntp-ods-dev.iam.gserviceaccount.com"
